831c7849e5a563bd90b292797657e0c9e74a3170
Ahasan Haque <ahasan.hq.bd@gmail.com>
2022-04-16 16:07:09 +0600

    Celloscope

---
+++ 1skeleton/skeleton/src/test/java/sboot/skeleton/SkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package sboot.skeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
